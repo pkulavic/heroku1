d8bf386c8a3002aacebe9fe2ed54bb4ca22d6b1a
pkulavic <31197315+pkulavic@users.noreply.github.com>
2017-08-20 23:44:54 -0700

    Add files via upload

---
+++ pract_2/urls.py
@@ -0,0 +1,28 @@
+"""pract_2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from home.views import HomeTemplateView
+from find.views import find, find_detail_view
+from payment.views import checkout
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeTemplateView.as_view()),
+    url(r'^find/$', find),
+    url(r'^find/(?P<slug>[-\w]+)/$', find_detail_view),
+    url(r'^checkout/$', checkout),
+]
